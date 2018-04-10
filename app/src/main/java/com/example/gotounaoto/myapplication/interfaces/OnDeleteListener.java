63d50d01f68a0bbbad0ee8f9dbb37d1596b31b03
naomath <math1972naonao@gmail.com>
2018-04-10 22:07:23 +0900

    エラー発生

--- app/src/main/java/com/example/gotounaoto/myapplication/interfaces/OnDeleteListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.gotounaoto.myapplication.interfaces;
-
-/**
- * Created by gotounaoto on 2018/01/05.
- */
-
-public interface OnDeleteListener {
-    void deleteBook();
-}
--- app/src/main/java/com/example/gotounaoto/myapplication/interfaces/OnSendIntListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.gotounaoto.myapplication.interfaces;
-
-/**
- * Created by gotounaoto on 2018/01/26.
- */
-
-public interface OnSendIntListener {
-    public int sendInt();
-}
